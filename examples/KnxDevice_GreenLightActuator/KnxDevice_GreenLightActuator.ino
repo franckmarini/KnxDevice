d07c1e4bd47caadb1300541b6cf4d320fc28cd9f
Franck MARINI <fm@liwan.fr>
2014-07-30 20:50:04 +0200

    GreenLightActuator & PushButton examples added and tested on Arduino Mini Pro.

---
+++ examples/KnxDevice_GreenLightActuator/KnxDevice_GreenLightActuator.ino
@@ -0,0 +1,45 @@
+// Green Light Actuator using KnxDevice library
+// 
+// Required environment :
+// - a KNX network with a TPUART board and a push button device 
+// - Configure one of the push button channel in TOGGLE mode on EIB address 0.0.2 in ETS
+// - Arduino Mini Pro with its serial port connected to HW TPUART interface
+// - green and red leds plus pull-up resistors connected to digital ports 7&9
+
+#include <KnxDevice.h>
+
+int greenPin = 7; // green LED connected to pin 7 with active high
+int RedPin = 9; // red LED connected to pin 9 with active high
+
+// Definition of the Communication Objects attached to the device
+KnxComObject KnxDevice::_comObjectsList[] =
+{
+/* Index 0 : */ KnxComObject(G_ADDR(0,0,2), KNX_DPT_1_001 /* 1.001 B1 DPT_Switch */ , COM_OBJ_LOGIC_IN_INIT /* Logical Input Object with Init Read */ ) ,
+};
+
+const byte KnxDevice::_comObjectsNb = sizeof(_comObjectsList) / sizeof(KnxComObject); // do no change this code
+
+// Callback function to handle com objects updates
+void knxEvents(byte index) {
+  switch (index)
+  {
+    case 0 : // object index 0 has been updaed
+      if (Knx.read(0)) { digitalWrite(greenPin, HIGH); digitalWrite(RedPin,LOW); } // green led on, // red led off, 
+      else { digitalWrite(greenPin, LOW); digitalWrite(RedPin,HIGH); } // green led off, // red led on,
+      break;
+
+    default: break;      
+  }
+};
+
+
+void setup(){
+  pinMode(greenPin, OUTPUT); 
+  pinMode(RedPin, OUTPUT);
+  Knx.begin(Serial, P_ADDR(1,1,0xAA)); // start a KnxDevice session with physical address 1.1.AA on Serial UART
+}
+
+
+void loop(){ 
+  Knx.task();
+}
---
+++ examples/KnxDevice_PushButton/KnxDevice_PushButton.ino
@@ -0,0 +1,52 @@
+// KNX toggle push button using KnxDevice library
+// The push button drives one channel of a switch actuator device.
+// The feedback status of the channel is considered : in this way, the toggle value is always correct and the actuator might be driven by several buttons.
+
+// Required environment :
+// - a KNX network with a TPUART board and a switch actuator device
+// - ComObject 0.0.1 switches one of the actuator channel (configuration performed with ETS)
+// - ComObject 0.0.2 is the feedback status of the channel  (configuration performed with ETS)
+// - Arduino Mini Pro with its serial port connected to HW TPUART interface
+// - a push button conected to digital port 2
+
+#include <KnxDevice.h>
+
+// Definition of the Communication Objects attached to the device
+KnxComObject KnxDevice::_comObjectsList[] =
+{
+  /* Index 0 */ KnxComObject(G_ADDR(0,0,1), KNX_DPT_1_001 /* 1.001 B1 DPT_Switch */ , COM_OBJ_SENSOR  /* Sensor Output */ ) ,
+  /* Index 1 */ KnxComObject(G_ADDR(0,0,2), KNX_DPT_1_001 /* 1.001 B1 DPT_Switch */ , COM_OBJ_LOGIC_IN_INIT /* Logical Input Object with Init Read */) , 
+};
+
+const byte KnxDevice::_comObjectsNb = sizeof(_comObjectsList) / sizeof(KnxComObject); // do no change this code
+
+// function and variables to manage push button signal debounce
+static inline word TimeDeltaWord(word now, word before) { return (word)(now - before); }
+
+boolean pressed = false, debounce = false;
+word debounceStartTime;
+
+
+// Callback function to handle com objects updates
+void knxEvents(byte index) {}
+
+
+void setup(){
+  attachInterrupt(0 /* int. 0 on pin 2 */, ButtonReleased, FALLING);
+  Knx.begin(Serial, P_ADDR(1,1,0xAA)); // start a KnxDevice session with physical address 1.1.AA on Serial UART
+}
+
+
+void loop(){ 
+  Knx.task();
+  if (debounce && (TimeDeltaWord((word)millis(), debounceStartTime)> 500 /* ms */)) debounce = false;
+  if (pressed) { pressed=false; Knx.write(0,!Knx.read(1)); }
+}
+
+
+
+void ButtonReleased()
+{
+  if(!debounce) { pressed = true; debounce = true; debounceStartTime = (word) millis();}
+}
+
